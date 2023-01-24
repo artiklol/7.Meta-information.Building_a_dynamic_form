9666a22321458397b80307851b2f56419a4634da
Artem Sulzhenko <artiklol2012@gmail.com>
2023-01-24 17:52:06 +0300

    ADDED project

---
+++ task_7/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  task_7
+//
+//  Created by Artem Sulzhenko on 24.01.2023.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        view.backgroundColor = .systemMint
+    }
+
+
+}
+
